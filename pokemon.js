432cfe2cf5d07243e954da9a1f73d1725714b74f
thassiov <tvmcarvalho@gmail.com>
2017-03-14 05:34:05 -0300

    Adicionando pergunta

--- pokemon.js
+++ pokemon.js
@@ -3,3 +3,20 @@
 * @desc Buscar informações sobre pokémon
 * @license MIT
     */
+
+'use strict';
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const pokemon = "Forneça o nome de um pokémon: ";
+
+rl.question(pokemon, function(ans) {
+    rl.close();
+    console.log(ans);
+});
+
